8d687ed2035bf43f0f15854d33338cf236404456
Fitsum <Fitsum@gmail.com>
2018-05-17 15:54:35 +0300

    The Second commit

--- src/fodemo/Fodemo.java
+++ src/fodemo/Fodemo.java
@@ -21,6 +21,7 @@ public class Fodemo {
         System.out.print("HWloow");
         System.out.print("HWloow");
         System.out.print("HWloow");
+        System.out.print("HWloow");
     }
     
 }
